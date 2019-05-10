f8e45acba7e31cfb2c2ccee5ce8754d805e5688f
shupeng <john.hi.gm@gmail.com>
2019-05-10 21:26:24 +0800

    Initial Commit

---
+++ JUtilities/JUtilities.h
@@ -0,0 +1,13 @@
+//
+//  JUtilities.h
+//  JUtilities
+//
+//  Created by shupeng on 2019/5/10.
+//  Copyright © 2019 shupeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JUtilities : NSObject
+
+@end
---
+++ JUtilities/JUtilities.m
@@ -0,0 +1,13 @@
+//
+//  JUtilities.m
+//  JUtilities
+//
+//  Created by shupeng on 2019/5/10.
+//  Copyright © 2019 shupeng. All rights reserved.
+//
+
+#import "JUtilities.h"
+
+@implementation JUtilities
+
+@end
